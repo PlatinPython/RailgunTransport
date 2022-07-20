bd17c5c80891e49aa096257f2976f2ed46acb18f
PlatinPython <robert.alexander.kaempf@gmail.com>
2022-07-20 17:17:59 +0200

    Add SavedData implementation for targets.

---
+++ src/main/java/platinpython/railguntransport/util/TargetSavedData.java
@@ -0,0 +1,58 @@
+package platinpython.railguntransport.util;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.NbtUtils;
+import net.minecraft.nbt.Tag;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.saveddata.SavedData;
+import net.minecraft.world.level.storage.DimensionDataStorage;
+import net.minecraft.world.phys.Vec3;
+import platinpython.railguntransport.RailgunTransport;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class TargetSavedData extends SavedData {
+    private static final String FILE_NAME = new ResourceLocation(RailgunTransport.MOD_ID, "targets").toString().replace(':', '_');
+    private final HashSet<BlockPos> targets = new HashSet<>();
+
+    public TargetSavedData() {
+        this.setDirty();
+    }
+
+    public static TargetSavedData get(DimensionDataStorage dimensionDataStorage) {
+        return dimensionDataStorage.computeIfAbsent(TargetSavedData::load, TargetSavedData::new, FILE_NAME);
+    }
+
+    public void add(BlockPos pos) {
+        this.targets.add(pos);
+    }
+
+    public void remove(BlockPos pos) {
+        this.targets.remove(pos);
+    }
+
+    public List<BlockPos> getReachablePositions(BlockPos centerPos) {
+        Vec3 center = Vec3.atCenterOf(centerPos);
+        return this.targets.stream().filter(pos -> pos.closerToCenterThan(center, 10_000F)).toList();
+    }
+
+    @Override
+    public CompoundTag save(CompoundTag compoundTag) {
+        ListTag listTag = new ListTag();
+        targets.forEach(pos -> listTag.add(NbtUtils.writeBlockPos(pos)));
+        compoundTag.put("Positions", listTag);
+        return compoundTag;
+    }
+
+    public static TargetSavedData load(CompoundTag compoundTag) {
+        TargetSavedData targetSavedData = new TargetSavedData();
+        ListTag listTag = compoundTag.getList("Positions", Tag.TAG_COMPOUND);
+        for (int i = 0; i < listTag.size(); i++) {
+            targetSavedData.targets.add(NbtUtils.readBlockPos(listTag.getCompound(i)));
+        }
+        return targetSavedData;
+    }
+}
