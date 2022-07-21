8d530cd44183e8c6f087a71558ae0d156a58d7b7
PlatinPython <robert.alexander.kaempf@gmail.com>
2022-07-21 21:09:22 +0200

    Add BE data.

--- src/main/java/platinpython/railguntransport/block/entity/RailgunBlockEntity.java
+++ src/main/java/platinpython/railguntransport/block/entity/RailgunBlockEntity.java
@@ -1,12 +1,49 @@
 package platinpython.railguntransport.block.entity;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtUtils;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import platinpython.railguntransport.RailgunTransport;
 import platinpython.railguntransport.util.registries.BlockEntityRegistry;
 
+import java.util.Optional;
+
 public class RailgunBlockEntity extends BlockEntity {
+    private Optional<BlockPos> selectedTarget = Optional.empty();
+
     public RailgunBlockEntity(BlockPos worldPosition, BlockState blockState) {
         super(BlockEntityRegistry.RAILGUN.get(), worldPosition, blockState);
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag tag) {
+        super.saveAdditional(tag);
+        this.selectedTarget.ifPresent(blockPos -> tag.put("selectedTarget", NbtUtils.writeBlockPos(blockPos)));
+    }
+
+    @Override
+    public void load(CompoundTag tag) {
+        super.load(tag);
+        if (tag.contains("selectedTarget")) {
+            this.selectedTarget = Optional.of(NbtUtils.readBlockPos(tag.getCompound("selectedTarget")));
+        }
+    }
+
+    public Optional<BlockPos> getSelectedTarget() {
+        return this.selectedTarget;
+    }
+
+    public void setSelectedTarget(Optional<BlockPos> selectedTarget) {
+        this.selectedTarget = selectedTarget;
+        this.setChanged();
+    }
+
+    public void tryCapsuleSend() {
+        this.selectedTarget.ifPresentOrElse(
+                blockPos -> RailgunTransport.LOGGER.info("Sending capsule to {}.", blockPos.toShortString()),
+                () -> RailgunTransport.LOGGER.info("Not sending capsule.")
+        );
     }
 }
--- src/main/java/platinpython/railguntransport/block/entity/TargetBlockEntity.java
+++ src/main/java/platinpython/railguntransport/block/entity/TargetBlockEntity.java
@@ -1,12 +1,29 @@
 package platinpython.railguntransport.block.entity;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import platinpython.railguntransport.util.registries.BlockEntityRegistry;
 
 public class TargetBlockEntity extends BlockEntity {
+    private boolean isFree = true;
+
     public TargetBlockEntity(BlockPos worldPosition, BlockState blockState) {
         super(BlockEntityRegistry.TARGET.get(), worldPosition, blockState);
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag tag) {
+        super.saveAdditional(tag);
+        tag.putBoolean("isFree", this.isFree);
+    }
+
+    @Override
+    public void load(CompoundTag tag) {
+        super.load(tag);
+        if (tag.contains("isFree")) {
+            this.isFree = tag.getBoolean("isFree");
+        }
     }
 }
