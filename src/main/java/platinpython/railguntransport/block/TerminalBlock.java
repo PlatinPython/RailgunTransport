3a7281f8b49ffa3855dff1d2c0f927b852afc56f
PlatinPython <robert.alexander.kaempf@gmail.com>
2022-08-01 00:29:39 +0200

    Add connection type to terminal

--- src/main/java/platinpython/railguntransport/block/TerminalBlock.java
+++ src/main/java/platinpython/railguntransport/block/TerminalBlock.java
@@ -9,6 +9,7 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Mirror;
@@ -25,9 +26,11 @@ import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.network.PacketDistributor;
 import org.jetbrains.annotations.Nullable;
 import platinpython.railguntransport.block.entity.TerminalBlockEntity;
+import platinpython.railguntransport.util.TerminalConnectionType;
 import platinpython.railguntransport.util.multiblock.MultiblockHelper;
 import platinpython.railguntransport.util.multiblock.MultiblockType;
 import platinpython.railguntransport.util.network.NetworkHandler;
@@ -39,8 +42,11 @@ import platinpython.railguntransport.util.saveddata.TargetSavedData;
 
 public class TerminalBlock extends BaseEntityBlock {
     public static final DirectionProperty HORIZONTAL_FACING = BlockStateProperties.HORIZONTAL_FACING;
-    public static final EnumProperty<MultiblockType> MULTIBLOCK_TYPE = EnumProperty.create("type",
+    public static final EnumProperty<MultiblockType> MULTIBLOCK_TYPE = EnumProperty.create("multiblock_type",
                                                                                            MultiblockType.class
+    );
+    public static final EnumProperty<TerminalConnectionType> CONNECTION_TYPE = EnumProperty.create("connection_type",
+                                                                                                   TerminalConnectionType.class
     );
 
     public TerminalBlock() {
@@ -52,7 +58,8 @@ public class TerminalBlock extends BaseEntityBlock {
         this.registerDefaultState(this.getStateDefinition()
                                       .any()
                                       .setValue(HORIZONTAL_FACING, Direction.NORTH)
-                                      .setValue(MULTIBLOCK_TYPE, MultiblockType.NONE));
+                                      .setValue(MULTIBLOCK_TYPE, MultiblockType.NONE)
+                                      .setValue(CONNECTION_TYPE, TerminalConnectionType.NONE));
     }
 
     @SuppressWarnings("deprecation")
@@ -64,18 +71,85 @@ public class TerminalBlock extends BaseEntityBlock {
     @SuppressWarnings("deprecation")
     @Override
     public BlockState mirror(BlockState state, Mirror mirror) {
-        return state.setValue(HORIZONTAL_FACING, mirror.mirror(state.getValue(HORIZONTAL_FACING)));
+        BlockState newState = state.setValue(HORIZONTAL_FACING, mirror.mirror(state.getValue(HORIZONTAL_FACING)));
+        return switch (mirror) {
+            case LEFT_RIGHT -> {
+                if (state.getValue(HORIZONTAL_FACING).getAxis() == Direction.Axis.X) {
+                    yield newState.setValue(CONNECTION_TYPE, state.getValue(CONNECTION_TYPE).toggleBoth());
+                } else {
+                    yield newState;
+                }
+            }
+            case FRONT_BACK -> {
+                if (state.getValue(HORIZONTAL_FACING).getAxis() == Direction.Axis.Z) {
+                    yield newState.setValue(CONNECTION_TYPE, state.getValue(CONNECTION_TYPE).toggleBoth());
+                } else {
+                    yield newState;
+                }
+            }
+            default -> newState;
+        };
     }
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(HORIZONTAL_FACING).add(MULTIBLOCK_TYPE);
+        builder.add(HORIZONTAL_FACING, MULTIBLOCK_TYPE, CONNECTION_TYPE);
     }
 
     @Nullable
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext context) {
-        return this.defaultBlockState().setValue(HORIZONTAL_FACING, context.getHorizontalDirection().getOpposite());
+        Direction direction = context.getHorizontalDirection().getOpposite();
+        BlockEntity leftBlockEntity = context.getLevel()
+                                             .getBlockEntity(
+                                                     context.getClickedPos().relative(direction.getClockWise()));
+        boolean left = leftBlockEntity != null && leftBlockEntity.getCapability(
+                CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, direction.getOpposite()).isPresent();
+
+        BlockEntity rightBlockEntity = context.getLevel()
+                                              .getBlockEntity(context.getClickedPos()
+                                                                     .relative(direction.getCounterClockWise()));
+        boolean right = rightBlockEntity != null && rightBlockEntity.getCapability(
+                CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, direction.getOpposite()).isPresent();
+
+        BlockState state = this.defaultBlockState().setValue(HORIZONTAL_FACING, direction);
+        if (left && right) {
+            return state.setValue(CONNECTION_TYPE, TerminalConnectionType.BOTH);
+        } else if (left) {
+            return state.setValue(CONNECTION_TYPE, TerminalConnectionType.LEFT);
+        } else if (right) {
+            return state.setValue(CONNECTION_TYPE, TerminalConnectionType.RIGHT);
+        } else {
+            return state.setValue(CONNECTION_TYPE, TerminalConnectionType.NONE);
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public BlockState updateShape(BlockState state, Direction direction, BlockState neighborState, LevelAccessor level,
+                                  BlockPos currentPos, BlockPos neighborPos) {
+        if (direction.getAxis().isHorizontal()) {
+            if (direction == state.getValue(HORIZONTAL_FACING).getClockWise()) {
+                BlockEntity blockEntity = level.getBlockEntity(neighborPos);
+                if (blockEntity != null && blockEntity.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY,
+                                                                     direction.getOpposite()
+                ).isPresent()) {
+                    return state.setValue(CONNECTION_TYPE, state.getValue(CONNECTION_TYPE).activateLeft());
+                } else {
+                    return state.setValue(CONNECTION_TYPE, state.getValue(CONNECTION_TYPE).deactivateLeft());
+                }
+            } else if (direction == state.getValue(HORIZONTAL_FACING).getCounterClockWise()) {
+                BlockEntity blockEntity = level.getBlockEntity(neighborPos);
+                if (blockEntity != null && blockEntity.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY,
+                                                                     direction.getOpposite()
+                ).isPresent()) {
+                    return state.setValue(CONNECTION_TYPE, state.getValue(CONNECTION_TYPE).activateRight());
+                } else {
+                    return state.setValue(CONNECTION_TYPE, state.getValue(CONNECTION_TYPE).deactivateRight());
+                }
+            }
+        }
+        return state;
     }
 
     @Nullable
