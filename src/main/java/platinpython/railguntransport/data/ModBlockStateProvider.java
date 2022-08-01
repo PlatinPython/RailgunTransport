786b26785533ad892631884d60c248784dcb387f
PlatinPython <robert.alexander.kaempf@gmail.com>
2022-08-01 06:33:10 +0200

    Add final capsule and terminal models

--- src/main/java/platinpython/railguntransport/data/ModBlockStateProvider.java
+++ src/main/java/platinpython/railguntransport/data/ModBlockStateProvider.java
@@ -2,18 +2,15 @@ package platinpython.railguntransport.data;
 
 import net.minecraft.core.Direction;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.client.model.generators.ConfiguredModel;
 import net.minecraftforge.client.model.generators.ModelProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
-import net.minecraftforge.registries.RegistryObject;
 import platinpython.railguntransport.RailgunTransport;
+import platinpython.railguntransport.block.CapsuleBlock;
+import platinpython.railguntransport.block.TerminalBlock;
 import platinpython.railguntransport.util.registries.BlockRegistry;
-
-import java.util.function.Function;
 
 public class ModBlockStateProvider extends BlockStateProvider {
     public ModBlockStateProvider(DataGenerator generator, ExistingFileHelper existingFileHelper) {
@@ -22,23 +19,34 @@ public class ModBlockStateProvider extends BlockStateProvider {
 
     @Override
     protected void registerStatesAndModels() {
-        registerStatesBlockAndItemModels(BlockRegistry.CAPSULE, state -> {
-            Direction dir = state.getValue(BlockStateProperties.FACING);
+        getVariantBuilder(BlockRegistry.CAPSULE.get()).forAllStates(state -> {
+            Direction dir = state.getValue(CapsuleBlock.FACING);
             return ConfiguredModel.builder()
-                                  .modelFile(models().getExistingFile(BlockRegistry.CAPSULE.getId()))
+                                  .modelFile(models().getExistingFile(new ResourceLocation(RailgunTransport.MOD_ID,
+                                                                                           ModelProvider.BLOCK_FOLDER + "/capsule/capsule"
+                                  )))
                                   .rotationX(dir == Direction.DOWN ? 180 : dir.getAxis().isHorizontal() ? 90 : 0)
                                   .rotationY(dir.getAxis().isVertical() ? 0 : (int) (dir.toYRot() + 180) % 360)
                                   .build();
         });
-    }
 
-    @SuppressWarnings("SameParameterValue")
-    private void registerStatesBlockAndItemModels(RegistryObject<Block> block,
-                                                  Function<BlockState, ConfiguredModel[]> mapper) {
-        getVariantBuilder(block.get()).forAllStates(mapper);
+        itemModels().withExistingParent(BlockRegistry.CAPSULE.getId().toString(),
+                                        modLoc(ModelProvider.BLOCK_FOLDER + "/capsule/capsule")
+        );
 
-        itemModels().withExistingParent(block.getId().toString(),
-                                        modLoc(ModelProvider.BLOCK_FOLDER + "/" + block.getId().getPath())
+        getVariantBuilder(BlockRegistry.TERMINAL.get()).forAllStates(state -> {
+            Direction dir = state.getValue(TerminalBlock.HORIZONTAL_FACING);
+            int yaw = (int) ((dir.toYRot() + 180) % 360);
+            return ConfiguredModel.builder()
+                                  .modelFile(models().getExistingFile(new ResourceLocation(RailgunTransport.MOD_ID,
+                                                                                           ModelProvider.BLOCK_FOLDER + "/terminal/terminal"
+                                  )))
+                                  .rotationY(yaw)
+                                  .build();
+        });
+
+        itemModels().withExistingParent(BlockRegistry.TERMINAL.getId().toString(),
+                                        modLoc(ModelProvider.BLOCK_FOLDER + "/terminal/terminal")
         );
     }
 }
